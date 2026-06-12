6a0e231bcb132990e410db0f6c44750cd51a2b2d
jagadisholi <olijagadish1@gamil.com>
2026-06-12 11:51:07 +0545

    js practice intro

---
+++ jestha_29/abc.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!");
+// let a = 10;
+// let b = 12;
+// let c  = a + b;
+// console.log("The sum of a and b is: " + c);
+
+const PI = 3.14;
+let a = 4;
+let area = PI * a * a;
+console.log("the area of circle is " + area);
+
+let result = "pass";
+console.log(result);
+
