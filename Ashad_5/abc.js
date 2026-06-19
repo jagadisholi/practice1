4b916f69f7e70c86b5ca4d44b1d8390c4a94f2ff
jagadisholi <olijagadish1@gamil.com>
2026-06-19 10:51:59 +0545

    object

---
+++ Ashad_5/abc.js
@@ -0,0 +1,55 @@
+// student object
+// const student = {
+//     firstName: "Jack",
+//     rollNo: 32 
+// };
+
+// console.log(student);
+
+// create person object
+const abc = {
+    name: "John",
+    age: 20
+};
+
+console.log(abc);
+
+
+const person = {
+    name: "Bobby",
+    hobby: "Dancing",
+};
+
+// modify property
+person.hobby = "Singing";
+
+// display the object
+console.log(person);
+
+
+
+const student = {
+    name: "John",
+    age: 20,
+};
+
+// add properties
+student.rollNo = 14;
+student.faculty = "Science";
+
+// display the object
+console.log(student);
+
+
+const employee = {
+    name: "Tony",
+    position: "Officer",
+    salary: 30000,
+};
+
+// delete object property
+delete employee.salary
+
+// display the object
+console.log(employee);
+
